dfff1efeacb76148c4dae4d715fafdc8540b10a9
Selassie1 <mrselasi@gmail.com>
2024-11-01 19:57:02 +0000

    Fixed eslint errors for first production build

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -18,12 +18,8 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const actionTypes = ['default', 'destructive', 'outline'] as const;
 
 let count = 0
 
