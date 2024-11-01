b2e312edb5a9ad142665c6d94c15cf05386e83ac
Selassie1 <mrselasi@gmail.com>
2024-11-01 19:55:36 +0000

    Fixed eslint errors for first production build

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -3,8 +3,10 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+}
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
